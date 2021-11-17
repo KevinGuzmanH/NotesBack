70222b0f6a63ce66c2615ce515fc8c2d0ecd9989
KevinGuzmanH <82849364+KevinGuzmanH@users.noreply.github.com>
2021-11-17 16:54:20 -0500

    fix error

--- src/main/java/com/kevinproject/backtienda/securityconf/WebConfig.java
+++ src/main/java/com/kevinproject/backtienda/securityconf/WebConfig.java
@@ -27,7 +27,7 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://noteskeviny.netlify.app");
+        config.addAllowedOrigin("*");
         config.setAllowedHeaders(Arrays.asList(
                 DOBEFORE,
                 USERNAME,
